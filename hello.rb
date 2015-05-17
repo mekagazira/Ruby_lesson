a568b10ea7c3521ee7c04fa53727ddf320d20ad1
mekagazira@local_vagrant <guvavaanniversarys@hotmail.co.jp>
2015-05-17 11:13:58 +0900

    Ruby lesson

--- hello.rb
+++ hello.rb
@@ -100,3 +100,178 @@ p s
 # 配列
 a = %W(a b c)
 p a
+
+# 条件分岐 if
+score = 80
+if score > 60
+  puts "OK"
+elsif (score > 40)
+  puts "soso..."
+else
+  puts "NG"
+end
+
+puts "OK" if score > 60
+
+=begin
+ false：false nil(オブジェクトが存在しない)
+ true：それ以外
+
+if x
+   puts "...."
+end
+=end
+
+# 条件演算子
+b = 10
+c = 20
+a = b > c ? b : c
+puts a
+
+# 多重代入
+d,e = 30,40;
+puts d
+puts e
+
+# 条件分岐 case文
+signal = "red"
+
+case signal
+when "red"
+  puts "stop"
+when "green","blue"
+  puts "go"
+when "yellow"
+  puts "caution"
+else
+  puts "wrong signal"
+end
+
+
+# 繰り返し処理
+# times
+3.times do
+  puts "hello"
+end
+
+10.times do |i|
+  if(i == 1)
+    next
+  end
+
+  puts "#{i}:hello"
+
+  if(i == 5)
+    break
+  end
+end
+
+# for
+for i in 0..2 do
+  puts i
+end
+
+for color in ["red","blue","yellow"] do
+  puts color
+end
+
+["red","blue","yellow"].each do |color|
+  puts color
+end
+# each
+{"red":200,"blue":100,"yellow":1000}.each do |indexcolor,valueprice|
+  puts "#{indexcolor} : #{valueprice}"
+end
+
+# 関数的メソッド
+def sayHi
+  puts "hello"
+end
+sayHi()
+
+def sayHi2(name)
+  puts "hello " + name
+end
+sayHi2("Tom")
+
+def sayHi3(name = "steve")
+  return "hello " + name
+end
+puts sayHi3()
+
+# クラス
+# クラス名：大文字始まりにする。
+class User
+
+  # クラス変数
+  @@user = 0
+
+  def initialize(name)
+    # インスタンス変数
+    @name = name
+    @@user += 1
+  end
+
+  # インスタンスメソッド
+  def sayHi
+    puts "hello, my name is #{@name}"
+  end
+
+  # クラスメソッド
+  def User.sayHello
+    puts "hello from User class #{@@user}"
+  end
+
+=begin
+  # getter
+  def name
+    @name
+  end
+  # setter
+  def setname(newname)
+    @name = newname
+  end
+
+  attr_reader :name
+  attr_writer :name
+=end
+  # setter/getterを自動生成する。
+  attr_accessor :name
+end
+
+
+User.sayHello()
+tom = User.new("Tom")
+bob = User.new("Bob")
+tom.sayHi()
+bob.sayHi()
+User.sayHello()
+
+# クラスの継承
+class SuperUser < User
+  def shout
+    puts "HELLO from #{@name}"
+  end
+end
+
+jacob = User.new("Jacob")
+noah = SuperUser.new("Noah")
+jacob.sayHi()
+noah.sayHi()
+noah.shout()
+
+SuperUser.sayHello()
+User.sayHello()
+
+
+# アクセサ
+# インスタンス変数にアクセスする。
+jacob.name="hoge"
+puts jacob.name
+
+# Timeクラス
+t = Time.now
+p t.year
+p t
+
+p t.strftime("%Y-%m-%d %H:%M:%S")
